e1302c2559fb286c6a46caf86e59082940cf55b9
Javafuel <Javafuel@github.com>
2019-06-07 00:55:39 +0000

    added ability to add todo list, add todo items to lists, delete todo items and bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+resources :todo_lists do
+  resources :todo_items
+  end
+  
+root "todo_lists#index"
+  end

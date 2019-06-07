70fc15d14ed83a6dca1fb977f3b2a3c6bd676631
Javafuel <Javafuel@github.com>
2019-06-07 01:06:42 +0000

    added ability to mark todo items as complete, and added a bit of styling to show the complete status

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
+    before_action :set_todo_item, except: [:create]
  
 def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -10,6 +11,10 @@ private
 
 def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+end
+
+def set_todo_item
+ @todo_item = @todo_list.todo_items.find(params[:id])
 end
 
 def todo_item_params
@@ -24,6 +29,11 @@ def destroy
     flash[:error] = "Todo List item could not be deleted."
  end
     redirect_to @todo_list 
+end
+
+def complete
+ @todo_item.update_attribute(:completed_at, Time.now)
+ redirect_to @todo_list, notice: "Todo item completed"
 end
 
 end
